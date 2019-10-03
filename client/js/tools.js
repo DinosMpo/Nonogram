ce272f60c04a51e13585c141ccb6c5862f9de787
BraVe <braveheart7591@yahoo.gr>
2019-10-03 23:30:22 +0300

    just delete a space

--- client/js/tools.js
+++ client/js/tools.js
@@ -20,7 +20,7 @@ function createSinglePlayerTools() {
 		singleplayer.appendChild(li);
 	}
 
-	for(let i=0; i<singlePlayerExtraTools.length; i++) {
+	for(let i=0; i<singlePlayerExtraTools.length; i++) {   
 		var li = document.createElement('li');
 		li.classList.add("extra-tool");
 		var div = document.createElement('div');
