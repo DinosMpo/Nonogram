bd1a93808b1d4c0b62d93b28765e8ebe5a708080
BraVe <braveheart7591@yahoo.gr>
2020-06-06 17:46:06 +0300

    create code for next multiplayer stage

--- client/js/init.js
+++ client/js/init.js
@@ -58,7 +58,7 @@ function createLevel(level, stage) {
 
 function createMultiplayerLevel() {
 	$("#singleplayer-tools").hide();
-	
+	multiplayerStageIndex = 0;
 	setTimeout(function(){ 
 		state = "multiplayer";
 		$('#game-lobbie').hide();
@@ -66,12 +66,9 @@ function createMultiplayerLevel() {
 		$("#container-tools").show();
 		$("#multiplayer-tools").show();
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-
 		container.style.transform = "translateX(-50%)"; // ksana kalumpraro to x coordinate
 		container.style.left = "50%";
-
-		nonogram = new Nonogram(multiplayerLevels['android']); // h pista tou multi
-
+		nonogram = new Nonogram(multiplayerLevels[multiplayerLevelNames[multiplayerStageIndex]]); // h pista tou multi
 		canvas.width = nonogram.width;
 		canvas.height = nonogram.height;
 		canvas.style.border = "1px solid black";
@@ -85,7 +82,36 @@ function createMultiplayerLevel() {
 			$("#waiting-screen").show();
 		}
 		resetTools("multiplayer");
+		$("#info-current-progress").text("");
+		$("#info-current-progress").text(nonogram.findProgress() + "%");
 		$("#clients-count").hide();
 	}, 3000);
 };
 
+function createNextMultiplayerStage() {
+	$('#multiplayer-next-stage-popup').show();
+	setTimeout(function(){ 
+		$('#multiplayer-next-stage-popup').hide();
+		nonogram = new Nonogram(multiplayerLevels[multiplayerLevelNames[multiplayerStageIndex]]); // h pista tou multi
+		canvas.width = nonogram.width;
+		canvas.height = nonogram.height;
+		ctx.clearRect(0, 0, innerWidth, innerHeight);
+		nonogram.drawGrid();
+		nonogram.drawRowNumbers();
+		nonogram.drawColumnNumbers();
+
+		resetTools("multiplayer");
+		$("#info-current-progress").text("");
+		$("#info-current-progress").text(nonogram.findProgress() + "%");
+		if(player == 'player1') {
+			wait = false;
+			turn = true;
+		}else {
+			wait = false;
+			turn = false;
+			$('#waiting-screen').show();
+		}
+
+	}, 3000);
+
+}
