ed3021194807f04588e7e9d80409be18992d7892
BraVe <braveheart7591@yahoo.gr>
2020-10-20 13:37:30 +0300

    big chances to the server code to make it more readable for me
    i add the events multiplayer game, exit multiplayer waiting lobby,

--- server/server.js
+++ server/server.js
@@ -20,6 +20,8 @@ let roomno = 1;
 io.on('connection', (sock) => {
 	//when a user connects to the server
 	let inRoom = 0;
+	let multiplayerGame;
+	let game; //Auth h metablhth einai gia na dhmiourgh8ei to game otan bre8ei o 2os paixths kai meta na stalei kai stous duo kai sthn sthn dunexeia na to apo8hkeusoume sto multiPlayerGame 
 	clients ++;
 	console.log('Someone connected');	
 	sock.emit('message', 'Hi you are connected'); // i don't used
@@ -38,45 +40,46 @@ io.on('connection', (sock) => {
 		//An to room uparxei kai exei parapanw apo 1 atoma
 		if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1) {
 			 roomno++;
-			 // console.log(roomno);
 		}
+
 		//When someone joins the waiting lobby
 		if(waitingPlayer) { // if there is a player that waits to play
 			// start game when 2 users join
-			inRoom = roomno;
-			sock.leave('all');
-			sock.join('room-'+inRoom);
+			inRoom = roomno; //Dhlwnoume san room to trexon domatio
+			sock.leave('all'); //Bgainei apo to domatio all kai
+			sock.join('room-'+inRoom); //Mpainei sto domatio pou 8a paiksei multi
 			// var game = new NonogramMultiplayerGame(waitingPlayer, sock, inRoom);
-			console.log("Ready to play");
-
+			// console.log("Ready to play");
 			// io.to("room-"+game._roomId).emit('event', "eee");
-			let game = {};
-			game.player1 = waitingPlayer.id;
-			game.player2 = sock.id;
-			game.room 	 = inRoom;
-			waitingPlayer.emit('player', 'player1');
-			sock.emit('player', 'player2');
-			io.to("room-"+inRoom).emit('multiplayer', game);
-			waitingPlayer = null;
-			sock.emit('wait');
-			sock.to("room-"+inRoom).broadcast.emit('can play');
+			let game = {}; //Dhmiourgoume to game pou exei
+			game.player1 = waitingPlayer.id; //player1
+			game.player2 = sock.id; //player2
+			game.room 	 = inRoom; // kai room
+			waitingPlayer.emit('player', 'player1'); //Stelnoume ston waitingPlayer oti einai o player1
+			sock.emit('player', 'player2'); //kai ston allon oti einai o player2
+			io.to("room-"+inRoom).emit('multiplayer', game); //Stelnoume sto domatio pou einai oi 2 paixtes to multiplayer game
+			waitingPlayer = null; //To kanoume null gia na mpei allos paixths na perimenei
+			sock.emit('wait'); //Stelnoume ston deutero paixth na perimenei thn seira tou
+			sock.to("room-"+inRoom).broadcast.emit('can play'); //Epeidh o prwtos paixths paizei prwtos tou stelnoume mhnuma oti mporei na paiksei
 		}else {
 			// if(game) {
 			// 	game = null;
 			// }
-
-			inRoom = roomno;
-			console.log(inRoom);
-
-			sock.leave('all');
-			sock.join("room-"+inRoom);
-			
+			//Enas paixths egine waitingPlayer gia to multiplayer
+			inRoom = roomno; //Dhlwnoume san room to trexon domatio
+			// console.log(inRoom);
+			sock.leave('all'); //Bgainei apo to domatio all kai
+			sock.join("room-"+inRoom); //Mpainei sto domatio pou 8a paiksei multi
 			//When first user connects
 			waitingPlayer = sock;
-			waitingPlayer.emit('room', inRoom);
-			waitingPlayer.emit('message', 'Waiting for an opponent');
+			waitingPlayer.emit('room', inRoom); //Stelnoume ston waitingPlayer to domatio tou , gia pio logo den 8umamai
+			waitingPlayer.emit('message', 'Waiting for an opponent'); //Tou stelnoume mhnuma oti perimenei paixth
 		}
 	});
+
+	sock.on('multiplayer game', (data)=> {
+		multiplayerGame = data;
+	})
 
 	sock.on('correct', (data) => {
 		io.to("room-"+inRoom).emit('correct');
@@ -102,65 +105,57 @@ io.on('connection', (sock) => {
 		// sock.to("room-"+inRoom).broadcast.emit('choice', data);
 		io.to("room-"+inRoom).emit('choice', data);
 	});
-
+	
+	//When the multiplayer game is finished
 	sock.on('multiplayer finished', () => {
 		sock.to("room-"+inRoom).broadcast.emit('multiplayer finished');
 	});
 
+	//When the player press the home button and exits the multiplayer lobby
 	sock.on('exit-multiplayer', (data) => {
-		// console.log("dsdsds");
-		// if(sock.id === data.player1) {
-		// 	data._player2.emit('exit-multiplayer', "Player 1 left the lobby");
-		// 	data._player1.leave('room-'+this._roomId);
-		// 	data._player1.join('all');
-		// 	data._player2.leave('room-'+this._roomId);
-		// 	data._player2.join('all');
-		// }else{
-		// 	data._player1.emit('exit-multiplayer', "Player 2 left the lobby");
-		// 	data._player1.leave('room-'+this._roomId);
-		// 	data._player1.join('all');
-		// 	data._player2.leave('room-'+this._roomId);
-		// 	data._player2.join('all');
-		// }
-
-		// console.log(io.nsps['/'].sockets[sock.id].leave('room-'+inRoom));
-
 		io.nsps['/'].sockets[sock.id].leave('room-'+inRoom);
-
+		io.nsps['/'].sockets[sock.id].join('all');
 		if(sock.id === data.player1) {
 			io.to(data.player2).emit('exit-multiplayer', 'Player 1 left the lobby...');
-			// io.to(data.player1).leave('room-'+data.room);
-			// io.to(data.player2).leave('room-'+data.room);
-			io.nsps['/'].sockets[data.player1].leave('room-'+inRoom);
 			io.nsps['/'].sockets[data.player2].leave('room-'+inRoom);
-			io.nsps['/'].sockets[data.player1].join('all');
 			io.nsps['/'].sockets[data.player2].join('all');
 		}else if(sock.id === data.player2) {
 			io.to(data.player1).emit('exit-multiplayer','Player 2 left the lobby...');
-			// io.to(data.player2).leave('room-'+data.room);
-			// io.to(data.player1).leave('room-'+data.room);
-			io.nsps['/'].sockets[data.player2].leave('room-'+inRoom);
 			io.nsps['/'].sockets[data.player1].leave('room-'+inRoom);
-			io.nsps['/'].sockets[data.player2].join('all');
 			io.nsps['/'].sockets[data.player1].join('all');
 		}
-
+		inRoom = 0;
 	});
 
-	sock.on('exit', () => {
+	//When the player exits the waiting lobby of multiplayer
+	sock.on('exit multiplayer waiting lobby', () => {
 		waitingPlayer = null;
 		sock.leave("room-"+inRoom);
+		inRoom = 0;
 	});
 
-	//Message to all connected clients
-	io.sockets.emit('broadcast', { description: 'Players online: ' + clients });
+	//When a player left the lobby
+	sock.on('player-left', () => {
+		iRoom =0;
+		io.nsps['/'].sockets[sock.id].leave('room-'+inRoom);
+		io.nsps['/'].sockets[sock.id].join('all');
+	});
 
+	//When the player close the window or disconnect
 	sock.on('disconnect', () => {
 		clients --;
 	    console.log('user disconnected');
 		io.sockets.emit('broadcast', { description: 'Players online: ' + clients });
 		if(inRoom != 0) {
-			sock.leave("room-"+inRoom);
+			if(multiplayerGame) {
+				if(sock.id === multiplayerGame.player1) {
+					//Prepei na ta diaxwrhsw ta events kai na mhn kanw kai edw exit multiplayer
+					io.to(multiplayerGame.player2).emit('player-left', 'Player 1 left the lobby...');
+				}else if(sock.id === multiplayerGame.player2) {
+					//Prepei na ta diaxwrhsw ta events kai na mhn kanw kai edw exit multiplayer
+					io.to(multiplayerGame.player1).emit('player-left','Player 2 left the lobby...');
+				}
+			}
 		}
 	});
 
@@ -185,6 +180,8 @@ io.on('connection', (sock) => {
 		// }
 	});
 
+	//Message to all connected clients
+	io.sockets.emit('broadcast', { description: 'Players online: ' + clients });
 });
 
 server.on('error', (err) => {
